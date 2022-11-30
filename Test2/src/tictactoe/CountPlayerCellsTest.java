219d0d5136732e54c5996047c33b784393532289
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2022-11-30 16:56:06 +0100

    Upgrade CountPlayerCellsTest and GameDataTest to JUnit5

--- Test2/src/tictactoe/CountPlayerCellsTest.java
+++ Test2/src/tictactoe/CountPlayerCellsTest.java
@@ -1,24 +1,27 @@
 package tictactoe;
 
-import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
-public class CountPlayerCellsTest extends TestCase {
+public class CountPlayerCellsTest {
 
+    @Test
     public void test_should_detect_3_cells_of_player_x() {
         final var stateLine = "_O_X__X_X";
         final var player = Player.X;
 
         final int result = Game.countPlayerCells(player, stateLine);
 
-        assertEquals(3, result);
+        Assertions.assertEquals(3, result);
     }
 
+    @Test
     public void test_should_detect_1_cell_of_player_o() {
         final var stateLine = "_O_X__X_X";
         final var player = Player.O;
 
         final int result = Game.countPlayerCells(player, stateLine);
 
-        assertEquals(1, result);
+        Assertions.assertEquals(1, result);
     }
 }
--- Test2/src/tictactoe/gamestate/GameDataTest.java
+++ Test2/src/tictactoe/gamestate/GameDataTest.java
@@ -1,24 +1,26 @@
 package tictactoe.gamestate;
 
-import junit.framework.TestCase;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import tictactoe.Player;
 
-public class GameDataTest extends TestCase {
+public class GameDataTest {
 
-    public static class AddMoveTest extends TestCase {
-        public void test_should_add_move() {
+    @Nested
+    class AddMoveTest {
+        @Test
+        void test_should_add_move() {
             final var gameData = new GameData("_________");
             final var expected = "X___O____";
 
             gameData.addMove(0, 0, Player.X);
             gameData.addMove(1, 1, Player.O);
             final var actual = gameData.getGameStateLine();                    // internally we use order of 'row, column'
-            System.out.println( "actual: " + actual);
+            System.out.println("actual: " + actual);
             assertEquals(expected, actual);
         }
-    }
-
-    public void testDummyTestForClassWithoutFurtherTests(){
-        assertTrue(true);
     }
 }
